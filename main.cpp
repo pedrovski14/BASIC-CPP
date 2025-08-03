da5e53cc004943c1c84068c54f9c651e7da815c5
Pedro Nunes <pedro.silva_1@outlook.com>
2025-08-03 16:30:47 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //isostream is a header file that contains functions for basic input put and output operations.
+
+int main(){
+    std::cout << "I like pizza!" <<std::endl; //what we will do in this lesson is to write some basic output. colons (2) and c out means character output togheter this means standart character output then brackets and quotation mark and the sentence. Then run it.
+    std::cout << "It's really good!" <<std::endl;//endl indicate the of a line.
+    return 0; //we will want to return zero, if we reach return zero it means there is no problems in this program, however if one is return that means there was a problem.
+}
