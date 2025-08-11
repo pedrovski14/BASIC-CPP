d52c7decf4efdeab0971c4d8a1ad06b6b6712f79
Pedro Nunes <pedro.silva_1@outlook.com>
2025-08-11 13:16:29 -0300

    Add files via upload

---
+++ arithmetic operators.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(){
+
+//arithmetic operations = retunr a result of a specific arithemetic operation (+ - / *)
+//int students = 20;
+//order of the operations:
+//parenthesis
+//multiplication and division
+//addition and subtraction
+
+
+
+
+
+double students = 6 - 5 + 4 * 3 / 2;
+
+//students = students + 5;
+//students+=1;
+//students++;
+
+//students = students - 5;
+//students-=1;
+//students++;
+
+//students = students * 2;
+//students*=2;
+
+//students = students / 3;
+//students/=2;
+//
+
+
+std::cout <<  "the number of students are:" << students;
+
+return 0;
+}
